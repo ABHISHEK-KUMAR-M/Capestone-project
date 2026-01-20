2d5112b77d718cd5e46c02c9ab8f4c965413c3be
Shwetanjali <shwetanjali05@gmail.com>
2026-01-20 18:09:52 +0530

    added ticket reply component and service

--- TicketPortal/src/app/ticketreply-component/ticketreply-component.ts
+++ TicketPortal/src/app/ticketreply-component/ticketreply-component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TicketReply } from '../../Models/ticketreply';
+import { TicketreplyService } from '../ticketreply-service';
 
 @Component({
   selector: 'app-ticketreply-component',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './ticketreply-component.html',
   styleUrl: './ticketreply-component.css',
 })
-export class TicketreplyComponent {
 
+export class TicketreplyComponent {
+  replySvc: TicketreplyService = inject(TicketreplyService);
+
+  reply: TicketReply;
+  replies: TicketReply[];
+  errMsg: string;
+
+  empId: string;
+  role: string | null;
+
+  constructor() {
+    this.reply = new TicketReply();
+    this.replies = [];
+    this.errMsg = '';
+
+    this.empId = sessionStorage.getItem('empId') || '';
+    this.role = sessionStorage.getItem('role');
+  }
+
+  newReply() {
+    this.reply = new TicketReply();
+  }
+
+  loadRepliesByTicket() {
+    if (!this.reply.ticketId) {
+      this.errMsg = 'Enter Ticket ID';
+      return;
+    }
+
+    this.replySvc.getRepliesByTicket(this.reply.ticketId).subscribe({
+      next: (res) => {
+        this.replies = res;
+        this.errMsg = '';
+      },
+      error: (err) => (this.errMsg = err.error),
+    });
+  }
+
+  sendReply() {
+    if (this.role === 'Creator') {
+      this.reply.repliedByCreatorEmpId = this.empId;
+      this.reply.repliedByAssignedEmpId = undefined;
+    } else {
+      this.reply.repliedByAssignedEmpId = this.empId;
+      this.reply.repliedByCreatorEmpId = undefined;
+    }
+
+    this.replySvc.addReply(this.reply).subscribe({
+      next: () => {
+        alert('Reply Sent');
+        this.loadRepliesByTicket();
+        this.reply.message = '';
+      },
+      error: (err) => (this.errMsg = err.error),
+    });
+  }
+
+  deleteReply(replyId: number) {
+    this.replySvc.deleteReply(replyId).subscribe({
+      next: () => {
+        alert('Reply Deleted');
+        this.loadRepliesByTicket();
+      },
+      error: (err) => (this.errMsg = err.error),
+    });
+  }
 }
+
--- TicketPortal/src/app/ticketreply-service.ts
+++ TicketPortal/src/app/ticketreply-service.ts
@@ -1,8 +1,57 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TicketReply } from '../Models/ticketreply';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TicketreplyService {
-  
+  http: HttpClient = inject(HttpClient);
+
+  token;
+  baseUrl: string = 'http://localhost:5181/api/TicketReply/';
+  httpOptions;
+
+  constructor() {
+    this.token = sessionStorage.getItem('token');
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.token,
+      }),
+    };
+  }
+
+  getAllReplies(): Observable<TicketReply[]> {
+    return this.http.get<TicketReply[]>(this.baseUrl, this.httpOptions);
+  }
+
+  getReplyById(replyId: number): Observable<TicketReply> {
+    return this.http.get<TicketReply>(
+      this.baseUrl + replyId,
+      this.httpOptions
+    );
+  }
+
+  getRepliesByTicket(ticketId: number): Observable<TicketReply[]> {
+    return this.http.get<TicketReply[]>(
+      this.baseUrl + 'ticket/' + ticketId,
+      this.httpOptions
+    );
+  }
+
+  addReply(reply: TicketReply): Observable<any> {
+    return this.http.post(this.baseUrl, reply, this.httpOptions);
+  }
+
+  updateReply(reply: TicketReply): Observable<any> {
+    return this.http.put(this.baseUrl, reply, this.httpOptions);
+  }
+
+  deleteReply(replyId: number): Observable<any> {
+    return this.http.delete(
+      this.baseUrl + replyId,
+      this.httpOptions
+    );
+  }
 }
